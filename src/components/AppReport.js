a51c10bb92fd3ea0888ce1e35f474166e414aace
Lion <lion@lion.com>
2023-05-14 12:23:46 +0330

    Add report section

---
+++ src/components/AppReport.js
@@ -0,0 +1,17 @@
+function AppReport() {
+    return (
+        <div className="p-report">
+            <div className="p-report__item">
+                <span>Pomodoro:</span>
+                <span className="p-report__value">2/2</span>
+            </div>
+            <div className="p-report__item">
+                <span>Finish At:</span>
+                <span className="p-report__value">10:24</span>
+                <span>(0h)</span>
+            </div>
+        </div>
+    )
+}
+
+export default AppReport
